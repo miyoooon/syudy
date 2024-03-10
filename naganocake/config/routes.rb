9e6202bbbb0b1aca3659ac08f0aa9f9679fa8a25
miyoooon <133766145+miyoooon@users.noreply.github.com>
2024-03-10 10:13:26 +0000

    [Add] devise, bootstrap, customer&admin/model,controller,view

---
+++ naganocake/config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+
+  devise_for :customers, skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+
+  devise_for :admins, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    resources :items
+  end
+end
